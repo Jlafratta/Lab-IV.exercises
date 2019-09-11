36227843bd4285487f3cdbd4b17c8cfcc1e3bb73
Jlafratta <52461452+Jlafratta@users.noreply.github.com>
2019-09-11 03:24:46 -0300

    .

--- TP 3.1/TP3.1.php
+++ TP 3.1/TP3.1.php
@@ -2,19 +2,22 @@
 <html>
     <head>
         <title>Laboratorio IV TP3.1</title>
+        <link rel="stylesheet" href="allTextStyle.css">
     </head>
     <body>
         <?php
+            
+            echo "<h1>Ejercicio 1</h1>";
+            echo "<br>";
+
             $name = 'juan,maria,pepe,andrea,jorgelina,cecilia';
             
             echo "String : " . $name;
             echo "<br><br>";
-            echo "Cantidad de palabras : " . str_word_count($name);
-            echo "<br><br>";
             
             $nameArray = explode(",", $name);
 
-            echo "Cantidad de elementos en el array : ". count($nameArray);
+            echo "<strong> a) </strong>Elementos en el array (nameArray): ". count($nameArray);
             echo "<br><br>";
 
             for($i=0; $i<count($nameArray); $i++)
@@ -23,10 +26,10 @@
                 echo "<br>";
             }
 
-            sort($nameArray, 2);
-
+            sort($nameArray, 2); // funcion que ordena el array. El segundo parametro (2) define de 
+                                 // que tipo son los elementos que contiene (numeros).
             echo "<br>";
-            echo "Array ordenado de manera ascendente: ";
+            echo "<strong> b) </strong>Array ordenado de manera ascendente: ";
             echo "<br><br>";
 
             for($i=0; $i<count($nameArray); $i++)
@@ -37,7 +40,7 @@
 
 
             echo "<br>";
-            echo "Primer letra de cada nombre en mayus (newArray): ";
+            echo "<strong> c) </strong>Primer letra de cada nombre en mayus (newArray): ";
             echo "<br><br>";
 
             $newArray;
@@ -52,6 +55,62 @@
                 echo "Name: " . $newArray[$i];
                 echo "<br>";
             }
+
+            echo "<br><strong> d) </strong> Cantidad de valores de newArray : " . count($newArray);
+
+            $keys = array();
+
+            for($i=0; $i<count($newArray); $i++)
+            {
+                $keys[$i] = $i+1;
+            }
+
+            echo "<br><br>";
+            echo "Arreglo keys : <br><br>";
+            
+            for($i=0; $i<count($keys); $i++)
+            {
+                echo $keys[$i];
+                echo "<br>";
+            }
+
+            $arrayAssociative = array_combine($keys, $newArray);
+
+            echo "<br><strong> e) </strong> arrayAssociative (keys + newArray) : <br><br>";
+
+            foreach($arrayAssociative as $x => $x_value)
+            {
+                echo " | Key: " . $x . " |  Value: " . $x_value . "<br>";
+            }
+
+            echo "<hr/>";
+            echo "<h1>Ejercicio 2</h1>";
+
+            $valueToFound = "Andrea";
+
+            echo "Array pasado por parametro => nameArray";
+            echo "<br> Valor pasado por parametro => '$valueToFound' <br><br> ";
+
+            function existsInArray($array, $element)
+            {
+                for($i=0; $i<count($array); $i++)
+                {
+                    if($array[$i] == $element)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            if(existsInArray($nameArray, $valueToFound))
+            {
+                echo "El elemento '$valueToFound' se encuentra dentro del array";
+            }else{
+                echo "El elemento '$valueToFound' no se encuentra dentro del array";
+            }
+
+
         ?>
     </body>
 </html>
